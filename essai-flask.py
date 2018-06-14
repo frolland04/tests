ea5340e99a254015a4aa3bb1be61c31390477b12
frolland04 <frederic.rolland@free.fr>
2018-06-14 22:01:18 +0200

    premiers essais flask faits à maison

---
+++ essai-flask.py
@@ -0,0 +1,43 @@
+import flask
+import flask_bootstrap
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def www_slash() -> str:
+    app.logger.debug('slash')
+    return 'coucou, bienvenue sur cette page!'
+
+
+@app.route('/test')
+def www_test() -> str:
+    app.logger.debug('test')
+    return 'Vous vouliez un test?!'
+
+
+@app.route('/test2')
+def www_test2() -> str:
+    app.logger.debug('test2')
+    return 'Vous vouliez vraiment un test?!'
+
+
+@app.route('/test3')
+def www_test3() -> str:
+    app.logger.debug('test3')
+    return flask.redirect(flask.url_for('www_test2'))
+
+
+@app.route('/test4')
+def www_test4() -> str:
+    app.logger.debug('test4')
+    return flask.redirect('/')
+
+
+@app.errorhandler(404)
+def page_not_found(error) -> str:
+    return 'Too bad!'
+
+
+if __name__ == '__main__':
+    app.run()
