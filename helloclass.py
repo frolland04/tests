f5b297a1fc09575150b5bba348eeb8e339de374e
frolland04 <frederic.rolland@free.fr>
2018-07-26 08:55:53 +0200

    nouveau fichier helloclass pour faire des essais sur les classes

---
+++ helloclass.py
@@ -0,0 +1,16 @@
+"""Quelques essais sur les classes Python.
+Je les mets sous forme de programme pour s'en souvenir facilement
+"""
+
+# Quelques informations
+__author__ = 'Frédéric ROLLAND'
+__version__ = '1'
+
+
+# Importations
+import sys
+import time
+
+if __name__ == "__main__":
+    print('coucou!')
+    print('fin.')
