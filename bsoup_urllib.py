3fe1e77a66f7e2083b9496383fa1bafb3e6f1cc3
frolland04 <frederic.rolland@free.fr>
2018-08-20 22:10:07 +0200

    tests faits à la maison

--- bsoup_urllib.py
+++ bsoup_urllib.py
@@ -1,3 +1,7 @@
+
+# python -m pip install --upgrade pip
+# python -m pip install --upgrade BeautifulSoup4
+
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.error
@@ -5,11 +9,16 @@ import re
 
 
 def fouine(a, links):
+    """Contacte et fouine dans l'URL web passée en 1er paramètre, rajoute dans le conteneur passé en 2ème paramètre
+    les URLs des balises HTML 'href' renvoyées dans la réponse par le serveur web que l'on contacte"""
     try:
+        # Contacte le serveur avec urllib et attend la réponse
         html_page = urllib.request.urlopen(a)
+        # Parse la réponse avec BeautifulSoup
         soup = BeautifulSoup(html_page)
 
         for link in soup.findAll('a', attrs={'href': re.compile("^http://|^https://")}):
+            # Ne rajoute les URLs que si elles n'y sont pas déjà
             if link.get('href') not in links:
                 links.append(link.get('href'))
     except:
@@ -23,46 +32,52 @@ if __name__ == "__main__":
     cible_index = 0
 
     # Ici on gardera les URL des documents à conserver
-    resultat = list()
+    resultats = list()
 
     fouine('https://python.developpez.com/cours', cibles)
 
     while (cible_index != len(cibles)) and (cible_index < 5000):
 
         lien = cibles[cible_index]
-        print(cible_index, len(cibles), len(resultat), lien)
+        print(cible_index, len(cibles), len(resultats), lien)
 
         if ('py' in lien or 'Py' in lien) and ('forums' not in lien) and ('actu' not in lien) and ('.developpez.com/' in lien):
-            if lien.endswith('.pdf'):
-                print('YEAH!', len(resultat), lien)
-                if lien not in resultat:
-                    resultat.append(lien)
+            # Il n'y a que les PDF et les ZIP qui m'intéresse
+            if lien.endswith('.pdf') or lien.endswith('.zip'):
+                print('YEAH!', len(resultats), lien)
+                # Si on a pas encore ce document, on l'ajoute aux résultats
+                if lien not in resultats:
+                    resultats.append(lien)
             else:
-                if not (lien.endswith('.zip') or lien.endswith('.azw') or lien.endswith('.mobi') or lien.endswith('.epub')):
+                # On ne fouine pas dans les documents complexes
+                if not (lien.endswith('.azw') or lien.endswith('.mobi') or lien.endswith('.epub')):
                     fouine(lien, cibles)
 
         cible_index += 1
 
     cibles.sort()
 
-    fichier_cible = open('cibles.txt', 'w')
+    # Construisons un fichier avec toutes les URLs traitées (même celles ignorées)
+    fichier_cibles = open('cibles.txt', 'w')
     for cible in cibles:
-        fichier_cible.write('%s\n' % cible)
+        fichier_cibles.write('%s\n' % cible)
 
-    resultat.sort()
+    resultats.sort()
 
-    fichier_resultat= open('resultat.txt', 'w')
-    for s in resultat:
+    # Construisons un fichier avec toutes les URLs des documents conservés
+    fichier_resultats= open('resultats.txt', 'w')
+    for s in resultats:
         print(s)
-        fichier_resultat.write('%s\n' % s)
+        fichier_resultats.write('%s\n' % s)
 
-    fichier_html = open('resultat.html', 'w')
-    for s in resultat:
+    # Le même sous forme de liens HTML
+    fichier_html = open('resultats.html', 'w')
+    for s in resultats:
         print(s)
         fichier_html.write("<a href='%s'>%s</a><br>\n" % (s,s))
 
-    fichier_cible.close()
-    fichier_resultat.close()
+    fichier_cibles.close()
+    fichier_resultats.close()
     fichier_html.close()
 
 
