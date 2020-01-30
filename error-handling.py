0d1b8ddf2772899dff4a09ed768b1c8b4c36afb3
Frédéric ROLLAND <frederic.rolland@pellenc.com>
2020-01-30 15:16:52 +0100

    Essai des context-managers (with, __enter__, __exit__) et petit beautifying des tests

--- error-handling.py
+++ error-handling.py
@@ -16,23 +16,45 @@ import sys
 
 class BigTest:
     def __init__(self, instance_id):
-        print('__init__', instance_id)
+        print('BigTest', '__init__()', instance_id)
         self.instance_id = instance_id
+        self.normal_exit_func = None
+        self.unexpected_exit_func = None
 
     def __del__(self):
-        print('__del__', self.instance_id)
+        print('BigTest', '__del__()', self.instance_id)
 
     def stuff(self, exc=None):
-        print('stuff', self.instance_id, exc)
+        print('BigTest', 'stuff', self.instance_id, exc)
         if exc is not None:
             raise exc
 
+    def register_normal_exit_func(self, func):
+        self.normal_exit_func = func
+
+    def register_unexpected_exit_func(self, func):
+        self.unexpected_exit_func = func
+
     def __enter__(self):
-        print('__enter__', self.instance_id)
+        print('BigTest', '__enter__()', self.instance_id)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('__exit__', self.instance_id, exc_type, exc_val, exc_tb)
+        print('BigTest', '__exit__()', self.instance_id, exc_type, exc_val, exc_tb)
+        if exc_val is None and self.normal_exit_func:
+            self.normal_exit_func()
+        else:
+            if exc_val is not None and self.unexpected_exit_func:
+                self.unexpected_exit_func()
+        return True
+
+
+def destroy_notification():
+    print('Boooom! Destruction!')
+
+
+def release_notification():
+    print('[Released.]')
 
 
 if __name__ == '__main__':
@@ -50,9 +72,21 @@ if __name__ == '__main__':
             with BigTest('e') as e:
                 print('e?')
 
+                with BigTest('z') as z:
+                    z.register_normal_exit_func(release_notification)
+                    z.register_unexpected_exit_func(destroy_notification)
+
     with BigTest('f') as f, BigTest('g') as g:
         print('f,g?')
+        f.register_normal_exit_func(release_notification)
+        f.register_unexpected_exit_func(destroy_notification)
+        g.register_normal_exit_func(release_notification)
+        g.register_unexpected_exit_func(destroy_notification)
+
         f.stuff()
-        g.stuff(SystemExit)
+        f.stuff(KeyboardInterrupt)
+
+        g.stuff()
+        # g.stuff(SystemExit)
 
     sys.exit(0)
